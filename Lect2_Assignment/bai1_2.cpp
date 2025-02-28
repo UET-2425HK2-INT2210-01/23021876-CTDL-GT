d9d919f3354d9efe3fce9eb6a6c8c3114d4d42b5
23021876_CTDLGT <23021876@vnu.edu.vn>
2025-02-28 23:14:27 +0700

    Add files via upload

---
+++ Lect2_Assignment/bai1_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int dem = 0;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[n];
+    }
+    for(int i=0; i<n; i++){
+        if(arr[i] = arr[i+1]){
+            dem++;
+            cout << dem++;
+        }
+    }
+}
